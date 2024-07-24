9cd5dcf6e65068005e669b1cc3311140aafbb4bd
Jeongho Nam <samchon.github@gmail.com>
2024-07-25 03:11:35 +0900

    Make concise

--- src/utils/ErrorUtil.ts
+++ src/utils/ErrorUtil.ts
@@ -1,9 +1,3 @@
-import fs from "fs";
-import { Singleton, randint } from "tstl";
-
-import { MyConfiguration } from "../MyConfiguration";
-import { DirectoryUtil } from "./DirectoryUtil";
-
 import serializeError = require("serialize-error");
 
 export namespace ErrorUtil {
@@ -12,38 +6,4 @@ export namespace ErrorUtil {
       ? err.toJSON()
       : serializeError(err);
   }
-
-  export async function log(
-    prefix: string,
-    data: string | object | Error,
-  ): Promise<void> {
-    try {
-      if (data instanceof Error) data = toJSON(data);
-
-      const date: Date = new Date();
-      const fileName: string = `${date.getFullYear()}${cipher(
-        date.getMonth() + 1,
-      )}${cipher(date.getDate())}${cipher(date.getHours())}${cipher(
-        date.getMinutes(),
-      )}${cipher(date.getSeconds())}.${randint(0, Number.MAX_SAFE_INTEGER)}`;
-      const content: string = JSON.stringify(data, null, 4);
-
-      await directory.get();
-      await fs.promises.writeFile(
-        `${MyConfiguration.ROOT}/logs/errors/${prefix}_${fileName}.log`,
-        content,
-        "utf8",
-      );
-    } catch {}
-  }
 }
-
-function cipher(val: number): string {
-  if (val < 10) return "0" + val;
-  else return String(val);
-}
-
-const directory = new Singleton(async () => {
-  await DirectoryUtil.mkdir(`${MyConfiguration.ROOT}/logs`);
-  await DirectoryUtil.mkdir(`${MyConfiguration.ROOT}/logs/errors`);
-});
